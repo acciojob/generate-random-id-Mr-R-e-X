d3cc0953cdfc3e7f5c912bbcb1af15793b5ff3f2
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-04-24 12:35:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
 function makeid(l) {
   // write your code here
+  if (l === 0) {
+    return "";
+  }
+  let randomChar = characters.charAt(
+    Math.floor(Math.random() * characters.length)
+  );
+  return randomChar + makeid(l - 1);
 }
 
-// Do not change the code below.
+// Do not change the code below. 
 const l = prompt("Enter a number.");
 alert(makeid(l));
